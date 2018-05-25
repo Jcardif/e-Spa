782dc912ba5f76c45b3c6136ba75612fcf900dc9
Jcardif <josh.cardif14@outlook.com>
2018-05-25 17:24:34 +0300

    Add project files.

---
+++ e-Spa Backend/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace e_Spa_Backend.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
---
+++ e-Spa Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(e_Spa_Backend.Startup))]
+
+namespace e_Spa_Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
