6503651090b452cba783af9e914eac1130f1ca27
Jcardif <josh.cardif14@outlook.com>
2018-07-14 21:29:37 +0300

    Add views

---
+++ e-Spa/e-SpaBackend/DataObjects/Views/ClientAppointments.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_SpaBackend.DataObjects
+{
+    public class ClientAppointments
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public string Venue { get; set; }
+        public Double Price { get; set; }
+        public Double Discount { get; set; }
+        public string SalonName { get; set; }
+        public string ServiceName { get; set; }
+        public string Image { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string SMFirstName { get; set; }
+        public string SMLastName { get; set; }
+    }
+}
--- e-Spa/e-SpaBackend/Migrations/Configuration.cs
+++ e-Spa/e-SpaBackend/Migrations/Configuration.cs
@@ -2,7 +2,9 @@ using Microsoft.Azure.Mobile.Server.Tables;
 using System;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace e_SpaBackend.Migrations
 {
@@ -15,22 +17,31 @@ namespace e_SpaBackend.Migrations
             AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
             SetSqlGenerator("System.Data.SqlClient", new EntityTableSqlGenerator());
+            ContextKey = "e_SpaBackend.Models.MobileServiceContext";
         }
 
         protected override void Seed(e_SpaBackend.Models.MobileServiceContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            //Seed the ClientAppointments_view script in the configuration file.
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var baseDirectory = Path.GetDirectoryName(path) + "\\Migrations\\ClientAppointmentsview.sql";
+            context.Database.ExecuteSqlCommand(File.ReadAllText(baseDirectory));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //Seed the SalonAppointments_view script in the configuration file.
+            string codeBase2 = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri2 = new UriBuilder(codeBase2);
+            string path2 = Uri.UnescapeDataString(uri2.Path);
+            var baseDirectory2 = Path.GetDirectoryName(path2) + "\\Migrations\\SalonAppointmentsview.sql";
+            context.Database.ExecuteSqlCommand(File.ReadAllText(baseDirectory2));
+
+            //Seed the SalonserviceView script in the configuration file.
+            string codeBase3 = Assembly.GetCallingAssembly().CodeBase;
+            UriBuilder uri3 = new UriBuilder(codeBase3);
+            string path3 = Uri.UnescapeDataString(uri3.Path);
+            var baseDirectory3 = Path.GetDirectoryName(path3) + "\\Migrations\\SalonServicesView.sql";
+            context.Database.ExecuteSqlCommand(File.ReadAllText(baseDirectory3));
         }
     }
 }
