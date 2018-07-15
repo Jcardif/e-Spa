0ad54763c3bbe61107fd1d0685a63bd968cbe197
Jcardif <josh.cardif14@outlook.com>
2018-07-15 11:56:29 +0300

    Add project

---
+++ e-Spa/e-SpaBackend/Controllers/Tables/SalonServiceController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using e_SpaBackend.DataObjects;
+using e_SpaBackend.Models;
+
+namespace e_SpaBackend.Controllers
+{
+    public class SalonServiceController : TableController<SalonService>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            MobileServiceContext context = new MobileServiceContext();
+            DomainManager = new EntityDomainManager<SalonService>(context, Request);
+        }
+
+        // GET tables/SalonService
+        public IQueryable<SalonService> GetAllSalonService()
+        {
+            return Query(); 
+        }
+
+        // GET tables/SalonService/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<SalonService> GetSalonService(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/SalonService/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<SalonService> PatchSalonService(string id, Delta<SalonService> patch)
+        {
+             return UpdateAsync(id, patch);
+        }
+
+        // POST tables/SalonService
+        public async Task<IHttpActionResult> PostSalonService(SalonService item)
+        {
+            SalonService current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/SalonService/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteSalonService(string id)
+        {
+             return DeleteAsync(id);
+        }
+    }
+}
---
+++ e-Spa/e-SpaBackend/DataObjects/Tables/Salon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.Mobile.Server;
+
+namespace e_SpaBackend.DataObjects
+{
+    public class Salon : EntityData
+    {
+        [Required, Column(TypeName = "VARCHAR")]
+        public string Deescription { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string Locality{ get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string ImageUrl { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string Email { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string Name { get; set; }
+        [Required, Column(TypeName = "INT")]
+        public int Rating { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string TimeIn { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string TimeOut { get; set; }
+        [Required, ForeignKey("SalonManager")]
+        public string SalonManager_Id { get; set; }
+
+        public virtual ICollection<Appointment> Appointments { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual SalonManager SalonManager { get; set; }
+        public virtual ICollection<SalonService> SalonServices { get; set; }
+    }
+}
