5b0d12cd6c3b4016426ce671865145b141146397
Jcardif <josh.cardif14@outlook.com>
2018-07-15 11:23:43 +0300

    fixed

---
+++ e-Spa/e-SpaBackend/DataObjects/Tables/SalonManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using Microsoft.Azure.Mobile.Server;
+
+namespace e_SpaBackend.DataObjects
+{
+    public class SalonManager : EntityData
+    {
+        [Required, Column(TypeName = "VARCHAR")]
+        public string FirstName { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string LastName { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string Email { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string ProfilePhotoUrl { get; set; }
+        [Required, Column(TypeName = "VARCHAR")]
+        public string PhoneNumber { get; set; }
+
+        public virtual  ICollection<Salon> Salons  { get; set; }
+    }
+}
---
+++ e-Spa/e-SpaBackend/DataObjects/Views/SalonServiceView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace e_SpaBackend.DataObjects
+{
+    public class SalonServiceView
+    {
+        public string Id { get; set; }
+        public string SalonName { get; set; }
+        public Double Price { get; set; }
+        public string Discount { get; set; }
+        public string ServiceName { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
