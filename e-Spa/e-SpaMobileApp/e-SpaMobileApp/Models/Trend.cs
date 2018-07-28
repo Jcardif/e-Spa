e2c1e4e1f58d05687030a97793a430f0cc5d1d56
Jcardif <josh.cardif14@outlook.com>
2018-07-29 02:57:30 +0300

    Models

---
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/Models/Trend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace e_SpaMobileApp.Models
+{
+    public class Trend
+    {
+        public string SenderName { get; set; }
+        public string TrendTitle { get; set; }
+        public string TrendDescripton { get; set; }
+        public string SenderProfileImageUrl { get; set; }
+
+    }
+}
