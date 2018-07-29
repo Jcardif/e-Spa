c7b1cd399c2d29d1ba81270333b5e83761b0cbc0
Jcardif <josh.cardif14@outlook.com>
2018-07-29 03:17:53 +0300

    Trends ViewHolder

--- e-Spa/e-SpaMobileApp/e-SpaMobileApp/Fragments/TrendsRecyclerViewAdapter.cs
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/Fragments/TrendsRecyclerViewAdapter.cs
@@ -10,11 +10,21 @@ using Android.Runtime;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
+using e_SpaMobileApp.Models;
 
 namespace e_SpaMobileApp.Fragments
 {
     public class TrendsRecyclerViewAdapter : RecyclerView.Adapter
     {
+        private List<Trend> _trends;
+        private Context _context;
+        private int _pos = -1;
+
+        public TrendsRecyclerViewAdapter(List<Trend>_trends, Context _context)
+        {
+            this._trends = _trends;
+            this._context = _context;
+        }
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
             throw new NotImplementedException();
---
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/TrendsRecyclerViewHolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Refractored.Controls;
+
+namespace e_SpaMobileApp
+{
+    public class TrendsRecyclerViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView TrendTitle { get; set; }
+        public TextView TrendDescription { get; set; }
+        public CircleImageView UserProfilePic { get; set; }
+        public TextView SenderName { get; set; }
+        public View TView { get; set; }
+
+        public TrendsRecyclerViewHolder(View itemView) : base(itemView)
+        {
+            TView = itemView;
+        }
+    }
+}
