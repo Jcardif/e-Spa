0e14b7c34665d07d44065688a97da12fffa62f45
Jcardif <josh.cardif14@outlook.com>
2018-07-31 07:38:42 +0300

    salon and services adapters and view holders

---
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/ViewHolders/SalonsRecyclerViewHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace e_SpaMobileApp.ViewHolders
+{
+    public class SalonsRecyclerViewHolder : RecyclerView.ViewHolder
+    {
+        public SalonsRecyclerViewHolder(View itemView) : base(itemView)
+        {
+        }
+    }
+}
