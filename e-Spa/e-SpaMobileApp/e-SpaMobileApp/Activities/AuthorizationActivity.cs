8618c7d62750385e31410fa15bac8bcb815a466e
Josh Cardif <josh.cardif14@outlook.com>
2018-09-03 11:24:58 +0300

    Authorzation Layouts

---
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/Activities/AuthorizationActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace e_SpaMobileApp.Activities
+{
+    [Activity(Label = "AuthorizationActivity")]
+    public class AuthorizationActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
