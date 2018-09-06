aa814e72f65aba7474ed895068cbaeff6a6903ef
Josh Cardif <josh.cardif14@outlook.com>
2018-09-06 12:26:37 +0300

    succesfuly send code

--- e-Spa/e-SpaMobileApp/e-SpaMobileApp/Activities/AuthorizationActivity.cs
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/Activities/AuthorizationActivity.cs
@@ -11,7 +11,11 @@ using Android.Runtime;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using e_SpaMobileApp.ExtensionsAndHelpers;
 using e_SpaMobileApp.Fragments;
+using Firebase;
+using Firebase.Auth;
+using Java.Util.Concurrent;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
@@ -21,16 +25,24 @@ namespace e_SpaMobileApp.Activities
     [Activity(Label = "@string/app_name", Theme = "@style/LogInTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
     public class AuthorizationActivity : AppCompatActivity
     {
-        private FrameLayout _authorisationFrameLayout;
-        public event EventHandler<string> CodeReceived; 
+        public event EventHandler<string> CodeReceived;
+        private FirebaseAuth _auth;
+        private FirebaseApp _app;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             AppCenter.Start("a90aca45-91cc-4e4f-80fe-bc7fffde8d57", typeof(Analytics), typeof(Crashes));
+            InitFirebaseAuth();
             SetContentView(Resource.Layout.activity_authorization);
-            _authorisationFrameLayout = FindViewById<FrameLayout>(Resource.Id.authorizationContainer);
             LoadFragment();
+        }
+
+        private void InitFirebaseAuth()
+        {
+            if (_app == null)
+                _app = FirebaseApp.InitializeApp(this);
+            _auth=new FirebaseAuth(_app);
         }
 
         private void LoadFragment()
@@ -43,7 +55,15 @@ namespace e_SpaMobileApp.Activities
 
         public  void OnVerificationAuthorized(object s, string phoneNo)
         {
-            Console.WriteLine($"verification authorised for {phoneNo}");
+            InitFirebaseAuth();
+            PhoneAuthCallBacks callBacks=new PhoneAuthCallBacks();
+            PhoneAuthProvider.GetInstance(_auth).VerifyPhoneNumber(
+                phoneNo,
+                2,
+                TimeUnit.Minutes,
+                this,
+                callBacks);
+
             OnCodeReceivedHandle("12qw4r47u7");
         }
 
