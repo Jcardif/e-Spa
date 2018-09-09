d94c75e4cc823a58c962007f29ebaf922fd55292
Josh Cardif <josh.cardif14@outlook.com>
2018-09-09 23:13:23 +0300

    Get user

---
+++ e-Spa/FunctionApp/UserExistence.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FunctionApp.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctionApp
+{
+    public static class UserExistence
+    {
+        [FunctionName("UserExistence")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            var connectionString =
+                Environment.GetEnvironmentVariable("DataConnectionString", EnvironmentVariableTarget.Process);  
+            var conn = new SqlConnection(connectionString);
+            try
+            {
+                await conn.OpenAsync();
+                var client = new Client();
+                var phoneNo = req.Content.ReadAsStringAsync();
+                var query = "SELECT * FROM Client WHERE PhoneNumber = +254742197114";
+                var cmd = new SqlCommand(query, conn);
+                var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    client.Id = reader.GetString(0);
+                    client.FirstName = reader.GetString(1);
+                    client.LastName = reader.GetString(2);
+                    client.Email = reader.GetString(3);
+                    client.PhoneNumber = reader.GetString(6);
+                    client.ProfilePhotoUrl = reader.GetString(4);
+                    client.Residence = reader.GetString(5);
+
+                    return req.CreateResponse<Client>(HttpStatusCode.OK,client);
+                }
+                else
+                {
+                    return req.CreateResponse<Client>(HttpStatusCode.NotFound,null);
+                }
+
+            }
+            catch (SqlException sqlex)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest,
+                    $"The following SqlException happened: {sqlex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest,
+                    $"The following Exception happened: {ex.Message}");
+            }
+        }
+    }
+}
