122adb142cd54b55a7423184d8caaca5f7a5be61
Josh Cardif <josh.cardif14@outlook.com>
2018-09-11 23:17:29 +0300

    Post to database

--- e-Spa/FunctionApp/AddNewUser.cs
+++ e-Spa/FunctionApp/AddNewUser.cs
@@ -9,27 +9,30 @@ using FunctionApp.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
 
 namespace FunctionApp
 {
     public static class AddNewUser
     {
         [FunctionName("AddNewUser")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req,[FromBody]Client client, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get","post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
             var connectionString =
                 Environment.GetEnvironmentVariable("DataConnectionString", EnvironmentVariableTarget.Process);
+            var client = await req.Content.ReadAsAsync<Client>();
+            //var client = JsonConvert.DeserializeObject<Client>(body as string);
             using (var conn=new SqlConnection(connectionString))
             {
                 conn.Open();
                 var query =
-                    $"INSERT INTO Client (firstName,lastName,email,phoneNumber,profilePhotoUrl,residence)" +
-                    $" VALUES ({client.FirstName},{client.LastName},{client.Email},{client.PhoneNumber}" +
-                    $",{client.ProfilePhotoUrl},{client.Residence}) SELECT SCOPE_IDENTITY()";
-                var cmd=new SqlCommand();
-                var lst = cmd.ExecuteScalar();
-                return req.CreateResponse(HttpStatusCode.OK, lst);
+                    $"INSERT INTO Client (FirstName,LastName,Email,PhoneNumber,ProfilePhotoUrl,Residence,Deleted)" +
+                    $" VALUES ('{client.FirstName}','{client.LastName}','{client.Email}','{client.PhoneNumber}'" +
+                    $",'{client.ProfilePhotoUrl}','{client.Residence}','false')";
+                var cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                return req.CreateResponse(HttpStatusCode.OK);
             }
 
         }
