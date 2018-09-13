596007d50b9d89d3a766ecf34de239a50941bc8b
Josh Cardif <josh.cardif14@outlook.com>
2018-09-13 12:12:15 +0300

    Add annotations

--- e-Spa/e-SpaMobileApp/e-SpaMobileApp/Fragments/CompleteRegistrationFragment.cs
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/Fragments/CompleteRegistrationFragment.cs
@@ -6,18 +6,26 @@ using System.Text;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
 using Android.Support.V4.App;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
 using e_SpaMobileApp.ServiceModels;
 using Newtonsoft.Json;
+using Refractored.Controls;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace e_SpaMobileApp.Fragments
 {
     public class CompleteRegistrationFragment : Fragment
     {
         private Client _client;
+        private Toolbar _toolbar;
+        private CircleImageView profilePicImgView;
+        private FloatingActionButton _fab;
+        private RelativeLayout dataContainerRelativeLayout;
+        private Button completeRegBtn;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,6 +37,14 @@ namespace e_SpaMobileApp.Fragments
         {
             base.OnCreateView(inflater, container, savedInstanceState);
             var view = inflater.Inflate(Resource.Layout.fragment_completeRegistration, container, false);
+            _toolbar = view.FindViewById<Toolbar>(Resource.Id.completeRegistrationToolbar);
+            profilePicImgView = view.FindViewById<CircleImageView>(Resource.Id.profilePictureCircularImgView);
+            _fab = view.FindViewById<FloatingActionButton>(Resource.Id.selectProfileImageFab);
+            dataContainerRelativeLayout = view.FindViewById<RelativeLayout>(Resource.Id.relativeDataformContainer);
+            completeRegBtn = view.FindViewById<Button>(Resource.Id.completeRegistartionBtn);
+
+            
+
             return view;
         }
     }
--- e-Spa/e-SpaMobileApp/e-SpaMobileApp/MobileClientServiceMangement/ServiceModels/Client.cs
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/MobileClientServiceMangement/ServiceModels/Client.cs
@@ -1,27 +1,32 @@
-﻿using Microsoft.WindowsAzure.MobileServices;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.WindowsAzure.MobileServices;
 using Newtonsoft.Json;
 
 namespace e_SpaMobileApp.ServiceModels
 {
     public class Client
     {
+        [Display(AutoGenerateField = false)]
         public string Id { get; set; }
+        [Display(Name ="First Name"), Required(AllowEmptyStrings =false, ErrorMessage ="First Name Cannot be empty")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name"), Required(AllowEmptyStrings = false, ErrorMessage = "Last Name Cannot be empty")]
         public string LastName { get; set; }
+        [Display(Name = "Email"), Required(AllowEmptyStrings = false, ErrorMessage = "Email Cannot be Empty")]
         public string Email { get; set; }
+        [Display(AutoGenerateField = false)]
         public string ProfilePhotoUrl { get; set; }
+        [Display(Name = "Residence"), Required(AllowEmptyStrings = false, ErrorMessage = "Residence Cannot be Empty")]
         public string Residence { get; set; }
+        [Display(Name = "Phone Number(eg 712345678)"), Required(AllowEmptyStrings = false, ErrorMessage = "Phone Number Cannot be Empty"), StringLength(10, ErrorMessage = "Enter a valid Phone Number")]
         public string PhoneNumber { get; set; }
 
-        [JsonIgnore]
-        public bool IsLoggedIn { get; set; }
+        [Version, Display(AutoGenerateField = false)] public string AzureVersion { get; set; }
 
-        [Version] public string AzureVersion { get; set; }
+        [CreatedAt, Display(AutoGenerateField = false)] public string AzureCreated { get; set; }
 
-        [CreatedAt] public string AzureCreated { get; set; }
+        [UpdatedAt, Display(AutoGenerateField = false)] public string AzureUpdated { get; set; }
 
-        [UpdatedAt] public string AzureUpdated { get; set; }
-
-        [Deleted] public string AzureDeleted { get; set; }
+        [Deleted, Display(AutoGenerateField = false)] public string AzureDeleted { get; set; }
     }
 }
