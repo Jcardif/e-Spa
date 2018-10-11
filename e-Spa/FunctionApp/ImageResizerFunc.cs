ceb15c4a4bf0d607723e1042a7c7928a6855a5b8
Josh Cardif <josh.cardif14@outlook.com>
2018-10-11 13:30:44 +0300

    fix

--- e-Spa/FunctionApp/ImageResizerFunc.cs
+++ e-Spa/FunctionApp/ImageResizerFunc.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using ImageResizer;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
 
 namespace FunctionApp
 {
@@ -12,14 +13,16 @@ namespace FunctionApp
         public static void Run(
             [BlobTrigger("espa-clients-profle-images/{name}", Connection = "BlobConnectionSstring")]Stream image,
             [Blob("espa-clients-profle-images-sm/{name}", FileAccess.Write, Connection = "BlobConnectionSstring")]Stream imageSmall,
-            [Blob("espa-clients-profle-images-md/{name}", FileAccess.Write, Connection = "BlobConnectionSstring")]Stream imageMedium)  // output blobs
+            [Blob("espa-clients-profle-images-md/{name}", FileAccess.Write, Connection = "BlobConnectionSstring")]Stream imageMedium, TraceWriter log) // output blobs
         {
+            log.Info("Image Resizer Processed an image");
             var imageBuilder = ImageBuilder.Current;
             var size = imageDimensionsTable[ImageSize.Small];
 
             imageBuilder.Build(
                 image, imageSmall,
                 new ResizeSettings(size.Item1, size.Item2, FitMode.Max, null), false);
+            log.Info("output to small");
 
             image.Position = 0;
             size = imageDimensionsTable[ImageSize.Medium];
@@ -27,6 +30,7 @@ namespace FunctionApp
             imageBuilder.Build(
                 image, imageMedium,
                 new ResizeSettings(size.Item1, size.Item2, FitMode.Max, null), false);
+            log.Info("Output to medium");
         }
 
         public enum ImageSize
