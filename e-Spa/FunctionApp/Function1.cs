a2cd4d5c3f3456ba615c3fdaa11d6ad00ace6cf8
Josh Cardif <josh.cardif14@outlook.com>
2018-10-11 19:08:02 +0300

    Add eventgrid trigger function

---
+++ e-Spa/FunctionApp/Function1.cs
@@ -0,0 +1,20 @@
+// This is the default URL for triggering event grid function in the local environment.
+// http://localhost:7071/admin/extensions/EventGridExtensionConfig?functionName={functionname} 
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FunctionApp
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([EventGridTrigger]JObject eventGridEvent, TraceWriter log)
+        {
+            log.Info(eventGridEvent.ToString(Formatting.Indented));
+        }
+    }
+}
