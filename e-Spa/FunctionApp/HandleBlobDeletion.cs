16998d09f6b7b6b4bf11f73de42fc0acadc462c1
Josh Cardif <josh.cardif14@outlook.com>
2018-10-12 18:31:27 +0300

    Wire up handle deletion

--- e-Spa/FunctionApp/HandleBlobDeletion.cs
+++ e-Spa/FunctionApp/HandleBlobDeletion.cs
@@ -1,9 +1,13 @@
 // This is the default URL for triggering event grid function in the local environment.
 // http://localhost:7071/admin/extensions/EventGridExtensionConfig?functionName={functionname} 
 
+using System;
+using System.Configuration;
+using FunctionApp.Models;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -12,9 +16,26 @@ namespace FunctionApp
     public static class HandleBlobDeletion
     {
         [FunctionName("HandleBlobDeletion")]
-        public static void Run([EventGridTrigger]JObject eventGridEvent, TraceWriter log)
+        public static void Run([EventGridTrigger]JObject eventGridEvent, ILogger log)
         {
-            log.Info(eventGridEvent.ToString(Formatting.Indented));
+            //log.LogInformation(eventGridEvent.ToString(Formatting.Indented));
+            var eventGridModel = JsonConvert.DeserializeObject<EventGridModel>(eventGridEvent.ToString());
+
+            log.LogInformation($"HandleBlobDeletion Function processed  {eventGridModel.Data.Url} has been deleted");
+
+            var mediumBlobUri =
+                eventGridModel.Data.Url.Replace("espa-clients-profle-images", "espa-clients-profle-images-md");
+            var smallBlobUri =
+                eventGridModel.Data.Url.Replace("espa-clients-profle-images", "espa-clients-profle-images-sm");
+
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["BlobConnectionSstring"].ConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var blobMedium = blobClient.GetBlobReferenceFromServer(new Uri(mediumBlobUri));
+            blobMedium.Delete();
+            log.LogInformation("Deleted in medium resoluion container");
+            var blobSmall = blobClient.GetBlobReferenceFromServer(new Uri(smallBlobUri));
+            blobSmall.Delete();
+            log.LogInformation("Deleted in Small reslution Container");
         }
     }
 }
