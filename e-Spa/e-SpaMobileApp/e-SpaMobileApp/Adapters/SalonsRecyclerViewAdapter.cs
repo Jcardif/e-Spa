6f3dbd177820e370d829560e9106f1989b03aa7c
Josh Cardif <josh.cardif14@outlook.com>
2018-10-22 19:25:22 +0300

    fetch salon images

--- e-Spa/e-SpaMobileApp/e-SpaMobileApp/Adapters/SalonsRecyclerViewAdapter.cs
+++ e-Spa/e-SpaMobileApp/e-SpaMobileApp/Adapters/SalonsRecyclerViewAdapter.cs
@@ -6,13 +6,16 @@ using Android.Widget;
 using e_SpaMobileApp.Models;
 using e_SpaMobileApp.ViewHolders;
 using Refractored.Controls;
+using Square.Picasso;
 
 namespace e_SpaMobileApp.Adapters
 {
-    public class SalonsRecyclerViewAdapter : RecyclerView.Adapter
+    public class SalonsRecyclerViewAdapter : RecyclerView.Adapter, ICallback
     {
         private Context _context;
         private readonly List<MySalon> _salons;
+        private RecyclerView.ViewHolder _holder;
+        private int _position;
 
         public SalonsRecyclerViewAdapter(Context context, List<MySalon> salons)
         {
@@ -24,12 +27,24 @@ namespace e_SpaMobileApp.Adapters
 
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
+            _holder = holder;
+            _position = position;
+            CustomBindViewHolder(holder, position);
+        }
+
+        private void CustomBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
             if (holder is SalonsRecyclerViewHolder vh)
             {
                 vh.SalonName.Text = _salons[position].SalonName;
                 vh.SalonManager.Text = _salons[position].SalonManager;
                 vh.Location.Text = _salons[position].Location;
                 vh.PhoneNo.Text = _salons[position].PhoneNumber;
+
+                Picasso.With(_context)
+                    .Load(_salons[position].SalonProfilePicUrl)
+                    .NetworkPolicy(NetworkPolicy.Offline)
+                    .Into(vh.SalonProfilePic, this);
             }
         }
 
@@ -51,6 +66,19 @@ namespace e_SpaMobileApp.Adapters
                 SalonName = salonNameTxtView
             };
             return viewHolder;
+        }
+
+        public void OnError()
+        {
+            Picasso.With(_context)
+                    .Load(_salons[_position].SalonProfilePicUrl)
+                    .Fetch();
+            OnBindViewHolder(_holder,_position);
+        }
+
+        public void OnSuccess()
+        {
+            
         }
     }
 }
